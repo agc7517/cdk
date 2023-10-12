f5d5c26db28e87fb8afa520e20dce832631245dd
Anand Achaibar <aachaibar@yahoo.com>
2023-10-12 01:09:39 -0400

    add task

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -3,7 +3,10 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import {aws_s3 as s3} from 'aws-cdk-lib';
 import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
-import { FargateCluster } from 'aws-cdk-lib/aws-eks';
+import cluster from 'cluster';
+import { Cluster, FargateTaskDefinition, TaskDefinition } from 'aws-cdk-lib/aws-ecs';
+import { cpuUsage } from 'process';
+
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -18,23 +21,35 @@ export class CdkStack extends cdk.Stack {
 
 
     const vpc = new Vpc(this, 'dotvpc', {
+      vpcName:'dot-vpc',
       cidr: '10.0.0.0/16',
       natGateways: 1,
       maxAzs: 2,
       subnetConfiguration: [
         {
-          name: 'private-subnet-1',
+          name: 'dot-private-subnet-1',
           subnetType: SubnetType.PRIVATE_WITH_EGRESS,
           cidrMask: 24,
         },
         {
-          name: 'public-subnet-1',
+          name: 'dot-public-subnet-1',
           subnetType: SubnetType.PUBLIC,
           cidrMask: 24,
         }
       ]
     });
 
+    const cluster = new Cluster(this, 'dot-cluster', {
+      vpc: vpc,
+    });
+
+    const taskDef = new FargateTaskDefinition(this, 'dot-task-def', {
+      cpu: 256,
+      memoryLimitMiB: 512,
+      
+    });
+
+    
 
 
     // example resource
